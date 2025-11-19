ea688d9ed931f6c318da79ee91cbd5165b8eca97
Anshoriee <kamalanshary123@gmail.com>
2025-11-19 21:02:47 +0700

    first commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCobwPcIwYOJKCOTW9DzSqslwuHE0Rp1QU",
-    authDomain: "logistik-dadec.firebaseapp.com",
-    projectId: "logistik-dadec",
-    storageBucket: "logistik-dadec.firebasestorage.app", // Perbaikan kecil: .appspot.com adalah format yang umum
-    messagingSenderId: "214169046309",
-    appId: "1:214169046309:web:1a4913c5007c5db5f29ecc",
-    measurementId: "G-ZLTH3B3GS8"
+    apiKey: "AIzaSyDV-fDvrlohSSpELfrB6Pdlsg5SypCvAHY",
+    authDomain: "logistik-himalaya-4dfb9.firebaseapp.com",
+    projectId: "logistik-himalaya-4dfb9",
+    storageBucket: "logistik-himalaya-4dfb9.firebasestorage.app",
+    messagingSenderId: "206816188774",
+    appId: "1:206816188774:web:b2b43a44c0d190bbe910a7",
+    measurementId: "G-4NR6L69534"
   };
